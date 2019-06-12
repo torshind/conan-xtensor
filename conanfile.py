b3fb31adedb88c22c437dc7f371add70d8469355
Marcello Mansueto <mvm@mydatamodels.com>
2019-06-12 15:04:14 +0200

    Removed git tools

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,25 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
 
 
 class XtensorConan(ConanFile):
     name = "xtensor"
     version = "0.20.7"
     license = "BSD 3-Clause"
-    url = "http://quantstack.net/xtensor"
+    homepage = "http://quantstack.net/xtensor/"
+    url = "https://github.com/torshind/conan-xtensor/"
     description = "C++ tensors with broadcasting and lazy computing"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
     generators = "cmake_find_package"
     exports_sources = "*"
-    requires = "xtl/0.6.4@mdm/testing", \
-               "xsimd/7.2.2@mdm/testing"
+    requires = "xtl/0.6.4@torshind/stable", \
+               "xsimd/7.2.3@torshind/stable"
 
     def source(self):
-        git = tools.Git()
-        git.clone("https://github.com/QuantStack/xtensor.git", self.version)
+        self.run("git clone --branch "
+                 + self.version
+                 + " https://github.com/QuantStack/xtensor.git")
 
     def build(self):
         pass
@@ -25,9 +27,8 @@ class XtensorConan(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.definitions["XTENSOR_USE_XSIMD"] = "ON"
-        cmake.configure()
+        cmake.configure(source_folder="xtensor")
         cmake.install()
 
     def package_info(self):
         self.info.header_only()
-        self.cpp_info.libs = ["xtensor"]
